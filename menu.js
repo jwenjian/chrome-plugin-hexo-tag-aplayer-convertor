47f81f26e941f45c6da2d488a4c0e54b90941dbd
jwenjian <jwenjian.me@gmail.com>
2018-12-24 20:33:29 +0800

    不支持的domain下弹窗提示并引导用户到readme页面 复制成功之后弹出提示

--- menu.js
+++ menu.js
@@ -1,14 +1,27 @@
+const NOTIFICATION_ID_UNSUPPORT_DOMAIN = "NOTIFICATION_ID_UNSUPPORT_DOMAIN";
+
 function onCopyMenuContextClick(info, tab) {
-  chrome.tabs.executeScript(
-    tab.id,
-    {
-      file: generateContentScriptJsName(info, tab)
-    },
-    copyTextToClipboard
-  );
+  let scritpFile = generateContentScriptJsName(info, tab);
+  if (scritpFile) {
+    chrome.tabs.executeScript(
+      tab.id,
+      {
+        file: scritpFile
+      },
+      copyTextToClipboard
+    );
+
+  }
+}
+
+function onNotificationClick(notificationId) {
+  if (notificationId.indexOf('UNSUPPORTED_DOMAIN_') !== -1) {
+    chrome.tabs.create({ url: 'https://github.com/jwenjian/chrome-plugin-hexo-tag-aplayer-convertor#supported-domains' });
+  }
 }
 
 chrome.contextMenus.onClicked.addListener(onCopyMenuContextClick);
+chrome.notifications.onClicked.addListener(onNotificationClick);
 
 chrome.contextMenus.create({ "id": "copy-as-aplayer-tag", "title": "Copy as aplayer tag", "contexts": ["page"] });
 chrome.contextMenus.create({ "id": "copy-as-aplayer-list-item", "title": "Copy as aplayer list item", "contexts": ["page"] });
@@ -21,12 +34,50 @@ chrome.contextMenus.create({ "id": "copy-as-aplayer-list-item", "title": "Copy a
  */
 function generateContentScriptJsName(info, tab) {
   let domain;
-  if (tab.url.indexOf('qq.com') > 0) {
+  if (tab.url.indexOf('y.qq.com/portal/player.html') !== -1) {
     domain = 'qq';
+  } else {
+    showUpsupportDomainNotificaction();
+    return null;
   }
+
 
   return `${domain}_${info.menuItemId}.js`;
 }
+
+
+
+function showUpsupportDomainNotificaction() {
+  let options = {
+    type: 'basic',
+    iconUrl: 'images/get_started48.png',
+    title: 'Convert failed!',
+    message: 'Current domain not supported yet, click [here] to see the supported domains.',
+    silent: true
+  };
+  chrome.notifications.create(
+    'UNSUPPORTED_DOMAIN_' + new Date().getTime(),
+    options,
+    null
+  );
+}
+
+function showCopySuccessNotification() {
+  let options = {
+    type: 'basic',
+    iconUrl: 'images/get_started48.png',
+    title: 'Converted and copied!',
+    message: 'Just go to your editor then press Ctrl-V.',
+    silent: true
+  };
+  chrome.notifications.create(
+    'CONVERTED_AND_COPIED_' + new Date().getTime(),
+    options,
+    null
+  );
+}
+
+console.log('ok');
 
 /**
  * Copy the input {text} to clipboard
@@ -62,4 +113,7 @@ function copyTextToClipboard(text) {
   //Remove the textbox field from the document.body, so no other JavaScript nor 
   //other elements can get access to this.
   document.body.removeChild(copyFrom);
+
+  // Show success notification
+  showCopySuccessNotification();
 }
